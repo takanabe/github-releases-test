b27a0d5cb8af8e225df505f630412a5b4ed3f3d1
Takayuki Watanabe <takanabe.w@gmail.com>
2021-11-02 15:47:44 +0900

    fix: valid regular expression for true/false detections

--- cmd/main.go
+++ cmd/main.go
@@ -16,18 +16,18 @@ var (
 	// target must specify release branch
 	validTarget = regexp.MustCompile(`^release-\d+$`)
 	// true or false
-	validPrerelease = regexp.MustCompile(`^{true|false}$`)
+	validPrerelease = regexp.MustCompile(`^(true|false)$`)
 )
 
 func main() {
 	fmt.Println("Start creating new release interactively!\n")
 
 	var tag string
-	fmt.Println("1. Input version tag with the format {major}.{minor}.{patch} or {major}.{minor}.{patch}-{rc#})")
+	fmt.Println("1. Input version tag with the format {major}.{minor}.{patch} or {major}.{minor}.{patch}-{rc#}")
 	fmt.Scanf("%s", &tag)
 
 	if !validTag.Match([]byte(tag)) {
-		fmt.Println("Version tag must follow the format {major}.{minor}.{patch} or {major}.{minor}.{patch}-{rc#})")
+		log.Fatal("Version tag must follow the format {major}.{minor}.{patch} or {major}.{minor}.{patch}-{rc#}")
 	}
 
 	var target string
@@ -35,7 +35,7 @@ func main() {
 	fmt.Scanf("%s", &target)
 
 	if !validTarget.Match([]byte(target)) {
-		fmt.Println("Target must follow the format release-#")
+		log.Fatal("Target must follow the format release-#")
 	}
 
 	var preRelease string
@@ -43,7 +43,7 @@ func main() {
 	fmt.Scanf("%s", &preRelease)
 
 	if !validPrerelease.Match([]byte(preRelease)) {
-		fmt.Println("Prerelease must be true or false")
+		log.Fatal("Prerelease must be true or false")
 	}
 
 	title := fmt.Sprintf("Release %s", tag)
